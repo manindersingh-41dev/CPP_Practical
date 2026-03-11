99b95e8b7760cbefdb39a79b0e163f2d4262bf68
Maninder Singh <manindersingh.41dev@gmail.com.com>
2026-03-11 15:39:26 +0530

    3rd lecture

---
+++ Control_flow.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 26;
+
+    // if (age>=18)
+    // {
+    //     cout<<"Eligible"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"not allowed"<<endl;
+    // }
+
+
+    int marks = 25;
+
+    // if (marks>90 && marks <= 100)
+    // {
+    //     cout<<"A"<<endl;
+    // }
+    // else if (marks >60 && marks <=90)
+    // {
+    //     cout<<"B"<<endl;
+    // }
+    // else if (marks>40 && marks <=60)
+    // {
+    //     cout<<"C"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"D"<<endl;
+    // }
+
+    return 0;
+
+}
--- operator.cpp
+++ operator.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a = 5,b=10;
+    int a = 2,b=3;
     double c;
     // c = a/b;
     // cout<<"Value of C: "<<c<<endl;
-    cout<<"Values: "<<a<<" "<<b<<endl;
+    // cout<<"Values: "<<a<<" "<<b<<endl;
     // add
     // cout<<a+b<<endl;
     // cout<<a-b<<endl;
@@ -38,19 +38,29 @@ int main()
 
     int d=5;
 
-    cout<< (a > b) <<endl;
-    // cout<<endl;
-    cout<< (a < b) <<endl;
+    // cout<< (a > b) <<endl;
+    // // cout<<endl;
+    // cout<< (a < b) <<endl;
 
-    cout<< (a==d) <<endl;
-    cout<< (a!=d) <<endl;
+    // cout<< (a==d) <<endl;
+    // cout<< (a!=d) <<endl;
 
-    cout<< (a>=d) <<endl;
-    cout<< (a<=d) <<endl;
+    // cout<< (a>=d) <<endl;
+    // cout<< (a<=d) <<endl;
 
 
     // Bitwise Operator
 
+    // Bitwise AND (&)
+
+    // cout<< (2&4)<<endl; 
+
+    // Bitwise OR
+    // cout<< (2 | 4)<<endl; 
+
+    // Left and Right shift
+    cout<< (2<<2) <<endl;
+    cout<< (2>>1) <<endl;
     
 
 
